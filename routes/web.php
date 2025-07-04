6a253a51ab8ccf78dd571e96c5cf02e7daedd167
Mikuru-creator <mikuru0101basket@icloud.com>
2025-07-04 12:26:28 +0900

    ルーティング設定
    ルーティングをweb.phpに追加

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students', 'StudentController@index'); // 学生一覧表示
+Route::get('/students/create', 'StudentController@create'); // 学生登録フォーム
+Route::post('/students', 'StudentController@store'); // 学生登録処理
+Route::get('/students/{id}/edit', 'StudentController@edit'); // 編集フォーム
+Route::post('/students/{id}', 'StudentController@update'); // 更新処理
+Route::post('/students/{id}/delete', 'StudentController@destroy'); // 削除処理
