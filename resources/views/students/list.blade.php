4cee118f3e98dacc0a0d27350e66d2d30956f55b
Mikuru-creator <mikuru0101basket@icloud.com>
2025-07-05 08:30:59 +0900

    テーブルの追加
    list.blade.phpにテーブルを追加

--- resources/views/students/list.blade.php
+++ resources/views/students/list.blade.php
@@ -6,5 +6,33 @@
 <body>
     <h1>学生一覧</h1>
     <a href="/students/create">新規登録</a>
+        <table border="1">
+        <thead>
+            <tr>
+                <th>名前</th>
+                <th>年齢</th>
+                <th>成績</th>
+                <th>操作</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->name }}</td>
+                    <td>{{ $student->age }}</td>
+                    <td>{{ $student->grade }}</td>
+                    <td>
+                        <a href="/students/{{ $student->id }}/edit">編集</a>
+                        <form action="/students/{{ $student->id }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit">削除</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
 </body>
 </html>
