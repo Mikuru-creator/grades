2f1ddf15adedf763ad7b2afcecbd06f294f56e72
Mikuru-creator <mikuru0101basket@icloud.com>
2025-07-04 12:55:49 +0900

    StudentControllerの作成
    学生の一覧・登録・編集・削除処理をコントローラーに記述

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+        return view('students.list', compact('students'));
+    }
+    public function create()
+    {
+        return view('students.create');
+    }
+        public function store(Request $request)
+    {
+        $student = new Student();
+        $student->name = $request->name;
+        $student->age = $request->age;
+        $student->grade = $request->grade;
+        $student->save();
+
+        return redirect('/students');
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::findOrFail($id);
+        $student->delete();
+
+        return redirect('/students');
+    }
+    public function edit($id)
+    {
+        $student = Student::findOrFail($id);
+        return view('students.edit', compact('student'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::findOrFail($id);
+        $student->name = $request->name;
+        $student->age = $request->age;
+        $student->grade = $request->grade;
+        $student->save();
+
+        return redirect('/students');
+    }
+}
